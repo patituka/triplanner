a8730a52854d9e97ee8c88aaef2c2d2b5eb988a5
patituka <patituka@hotmail.fr>
2022-04-19 14:04:51 +0200

    update

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,34 +1,19 @@
-import type { GatsbyConfig } from "gatsby";
 
-const config: GatsbyConfig = {
-  siteMetadata: {
+module.exports = {  siteMetadata: {
     title: `Triplanner`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-theme-ui",
-   "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
+  plugins: [
+    "gatsby-plugin-theme-ui",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
     }
-  }, 
-  "gatsby-plugin-mdx", 
-  "gatsby-plugin-sharp", 
-  "gatsby-transformer-sharp", 
-  {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-  }, 
-  {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-  }]
+  ]
 };
-
-export default config;
